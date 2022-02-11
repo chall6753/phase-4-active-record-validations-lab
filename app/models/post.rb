2d5695b7bac452a0ef67a2096742975cbb88c4d7
Charles <chall70101@gmail.com>
2022-02-10 20:38:27 -0700

    commit tree 459f97e416e60362d57927302945b5a5c589d7bc

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction","Non-Fiction"] }
+
+    validate :clickbait
+
+    def clickbait
+        
+        if title.include?("Won't Believe") 
+            true
+        else 
+            errors.add(:title, "not click baity")
+       
+        end
+    end
 end
